6ed4f66381ccbeecd36badd3d94b4b7d6119cdcf
avavion <avavionmvm@gmail.com>
2021-03-27 00:12:00 +0300

    Оживил слайдер

--- scripts/app.js
+++ scripts/app.js
@@ -1,8 +1,31 @@
 // Ждем пока строиться DOM
 window.addEventListener("DOMContentLoaded", () => {
-  document.head.insertAdjacentHTML(
-    "beforeend",
-    `<meta property="og:url" content="${location.protocol}//${location.hostname}/" />`
-  );
-  console.log(document.head);
+  const currentSlideCounter = document.querySelector(".hero-slider--currentCounter");
+
+  const heroSlider = new Swiper("#header-slider", {
+    loop: false,
+    speed: 2500,
+    init: true,
+    autoplay: {
+      delay: 1000,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      type: "progressbar",
+      clickable: true,
+    },
+  });
+
+  const changeCounter = (el, slider) => {
+    let currentSlide = slider.realIndex + 1;
+    el.textContent = currentSlide < 10 ? `0${currentSlide}` : `${currentSlide}`;
+  };
+
+  heroSlider.on("beforeInit", function () {
+    changeCounter(currentSlideCounter, heroSlider);
+  });
+
+  heroSlider.on("slideChange", function () {
+    changeCounter(currentSlideCounter, heroSlider);
+  });
 });

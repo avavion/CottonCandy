4d0ff17946a2cc32acd8947325e38721c5fb1bab
avavion <avavionmvm@gmail.com>
2021-03-29 19:26:22 +0300

    Сделал слайдер из галлереи, добавил паралакс и новые изображения.

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,18 @@
 // Ждем пока строиться DOM
 window.addEventListener("DOMContentLoaded", () => {
-  const currentSlideCounter = document.querySelector(".hero-slider--currentCounter");
+  // Вывод индекса текущего сладйа
+  /*
+   ** Принимает el - элемент, где изменяем текст;
+   ** slider - слайдер к которому привязываем;
+   */
 
+  const changeCounter = (el, slider) => {
+    const currentSlide = slider.realIndex + 1;
+    el.textContent = currentSlide < 10 ? `0${currentSlide}` : `${currentSlide}`;
+  };
+
+  // Секция Hero
+  const heroSlideCounter = document.querySelector(".hero-slider--currentCounter"); // Счётчик
   const heroSlider = new Swiper("#header-slider", {
     loop: false,
     speed: 2500,
@@ -16,16 +27,21 @@ window.addEventListener("DOMContentLoaded", () => {
     },
   });
 
-  const changeCounter = (el, slider) => {
-    let currentSlide = slider.realIndex + 1;
-    el.textContent = currentSlide < 10 ? `0${currentSlide}` : `${currentSlide}`;
-  };
+  heroSlider.on("beforeInit", () => changeCounter(heroSlideCounter, heroSlider));
+  heroSlider.on("slideChange", () => changeCounter(heroSlideCounter, heroSlider));
 
-  heroSlider.on("beforeInit", function () {
-    changeCounter(currentSlideCounter, heroSlider);
+  // Слайдер галлереи
+  const gallerySlider = new Swiper("#gallery-slider", {
+    loop: false,
+    speed: 2500,
+    init: true,
+    parallax: true,
+    spaceBetween: 5,
+    autoplay: {
+      delay: 5000,
+    },
   });
 
-  heroSlider.on("slideChange", function () {
-    changeCounter(currentSlideCounter, heroSlider);
-  });
+  gallerySlider.on("beforeInit", () => changeCounter(heroSlideCounter, gallerySlider));
+  gallerySlider.on("slideChange", () => changeCounter(heroSlideCounter, gallerySlider));
 });
